b766d75c759de39b59db617f6107a5776fa14e51
gsoosk <farzadhabibii98@gmail.com>
2020-06-10 23:55:55 +0430

    Merge squash

--- Q5.py
+++ Q5.py
@@ -10,20 +10,29 @@ import threading
 
 numberOfClient = 5
 protocolTag = '~'
-returnHome = False
-returnIp = 0
+returnIps = {}
+downloadedFiles = {}
+uploadedFiles = {}
 
 def splitFile(fileName):
-	f = open(fileName, 'rb')
+	global uploadedFiles 
+	uploadedFiles[fileName] = True
 	result = []
 	try:
+		f = open(fileName, 'rb')
 		byte = f.read(1)
-		result.append(str(byte))
+		result.append(byte)
 		while byte != "":
 			byte = f.read(1)
-			result.append(str(byte))
-	finally:
+			result.append(byte)
+		print "File uploaded succesfully."
 		f.close()
+<<<<<<< HEAD
+=======
+	except :
+		print "Can not upload."
+	result = result[:-1]
+>>>>>>> ed3514b... Downloaing multiple file and uploading multiple file added
 	return result
 
 def send():
@@ -105,9 +114,9 @@ def getRandomSourceAndDestination():
         return sourceIp, destinationIp
 
 def senderFunction(p):
-	fileName = raw_input("file name: ")
+	fileName = raw_input("File name: ")
 
-	fileData = splitFile(fileName)
+	fileData = splitFile(fileName)	
 
 	#send file
 	i = 0
@@ -128,38 +137,53 @@ def senderFunction(p):
 		i = i + 8
 
 def downloadFunction(p):
-	fileName = raw_input("file name: ")
+	global downloadedFiles
+	global uploadedFiles
+	fileName = raw_input("File name: ")
+	if(not fileName in uploadedFiles):
+		print "You can not downoad this file. Access denied."
+		return
 	ourIp = commands.getoutput('/sbin/ifconfig').split('\n')[1][20:28]
 	msg = 'return~'+ ourIp +'~0~0~0~' + fileName
 	src, dst = getRandomSourceAndDestination()
 	p.set_new_config(src, dst, msg)
 	p.do_send()
+	downloadedFiles[fileName] = {}
+#               0               1                              2          3       4           5            
+#Protocol : returnOrNot? ~ if return -> ip else -> data ~ numberOfData ~ size ~ toDelete ~ fileName 
 
 def receiverFunction(p):
-	global returnHome
-	global returnIp
-
+	global returnIps
 	packet_size , src_ip, dest_ip, ip_header, icmp_header , payLoad = p.do_receive()
-
 	if not packet_size == 0:
 		payloadData = payLoad.split('~')
-		if(payloadData[0] == 'return'): #If msg was return to home
-			returnIp = payloadData[1]
-			returnHome = True
-
+		 #If it was a downloaded data
+		if(icmp_header['type'] == ping.ICMP_ECHO and (not payloadData[0] == 'return') and payloadData[4] == '1'):
+			fileName = payloadData[5]
+			chunkNumber = int(payloadData[2])
+			print "***********Downloading data number %d for file %s"%(chunkNumber, fileName)
+			downloadedFiles[fileName][chunkNumber] = payloadData[1]
+			size = int(payloadData[3])
+			if(len(downloadedFiles[fileName]) == size):
+				packData(fileName, size)
+		#If msg was return to home
+		if(payloadData[0] == 'return'): 
+			returnIps[payloadData[5]] = payloadData[1]
 		if(icmp_header['type'] == ping.ICMP_ECHOREPLY):
 			# print "PayLoad is %s"%(payLoad)
+			#If Bezzy get the chunk
 			if(payloadData[4] == '1'):
-				print "***********Deleting"
-			elif(returnHome and (not payloadData[0] == 'return')):
+				print "***********Deleting file number %s from network"%(payloadData[2])
+			#If we should send this chunk to Home 
+			elif((not payloadData[0] == 'return') and payloadData[5] in returnIps):
 					payloadData[4] = '1'
 >>>>>>> 672afad... debug receiving function
 					payLoad = '~'.join(payloadData)
-					print "************Sending to Home %s"%(returnIp)
+					print "***********Sending to Home %s"%(returnIps[payloadData[5]])
 					ourIp = commands.getoutput('/sbin/ifconfig').split('\n')[1][20:28]
-					p.set_new_config(ourIp, returnIp, payLoad)
-					# time.sleep(1)
+					p.set_new_config(ourIp, returnIps[payloadData[5]], payLoad)
 					p.do_send()
+<<<<<<< HEAD
 <<<<<<< HEAD
 				else : 
 					sourceIp, destinationIp = getRandomSourceAndIp()
@@ -176,13 +200,38 @@ def getRandomSourceAndIp():
                 destinationIp = findRandomIp()
         return sourceIp, destinationIp
 =======
+=======
+			#If we should spin the chunk
+>>>>>>> ed3514b... Downloaing multiple file and uploading multiple file added
 			else : 
 				sourceIp, destinationIp = getRandomSourceAndDestination()
-				# print "random src is %s and dst is %s"%(sourceIp, destinationIp)
 				p.set_new_config(sourceIp, destinationIp, payLoad)
-				# time.sleep(1)
 				p.do_send()
 
+def packData(fileName, size):
+	print "**********Packing %s "%(fileName)
+	f = open('./downloaded_' + fileName, 'wb+')
+	for i in range(0, size):
+		f.write(downloadedFiles[fileName][i])
+	f.close()
+	
+def showFile():
+	fileName = raw_input("File name: ")
+	result = ""
+	try:
+		f = open(fileName, 'rb')
+		byte = f.read(1)
+		result += str(byte)
+		while byte != "":
+			byte = f.read(1)
+			result += str(byte)
+		print result
+		f.close()
+	except :
+		print "Can not open file."
+	result = result[:-1]
+	
+	
 def main():
 	p = Ping('0.0.0.0', '0.0.0.0')
 	currentSocket = p.get_socket()
@@ -201,6 +250,10 @@ def main():
 					senderFunction(p)
 				elif buffer[i] == "download":
 					downloadFunction(p)
+				elif buffer[i] == "vim":
+					showFile()
+				else :
+					print "Commmand not found! try again."
 			buffer = []
 >>>>>>> 672afad... debug receiving function
 	
